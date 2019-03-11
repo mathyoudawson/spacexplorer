b8b58a17098b936e899dd0be09be55723145ba08
Matthew D <matthewdawson@outlook.com>
2019-03-11 14:09:37 +1300

    Add functionality to wrapper to perform calls for all launches and a singular launch

--- app/apis/spacex_api/v3/client.rb
+++ app/apis/spacex_api/v3/client.rb
@@ -2,6 +2,34 @@ module SpacexApi
   module V3
     class Client
       API_ENDPOINT = 'https://api.spacexdata.com/v3'.freeze
+
+      def all_launches
+        request(
+          http_method: :get,
+          endpoint: "launches"
+        )
+      end
+
+      def one_launch(num)
+        request(
+          http_method: :get,
+          endpoint: "launches/#{num}"
+        )
+      end
+
+      private
+
+      def client
+        @_client ||= Faraday.new(API_ENDPOINT) do |client|
+          client.request :url_encoded
+          client.adapter Faraday.default_adapter
+        end
+      end
+
+      def request(http_method:, endpoint:, params: {})
+        response = client.public_send(http_method, endpoint, params)
+        Oj.load(response.body)
+      end
     end
   end
 end
