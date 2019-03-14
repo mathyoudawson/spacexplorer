a242a0a003a5daf77380bedcdc996cd19b338ad7
Matthew D <matthewdawson@outlook.com>
2019-03-14 15:44:20 +1300

    Show next launch as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :launches
   resources :next_launch
+
+  root 'next_launch#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
